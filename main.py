4b1e4dcedb2b440630348757cf1a94a85eb67baf
Todd Blank <toddcblank@gmail.com>
2021-01-13 21:09:35 -0600

    working with zoast/oats/behe bp vids, cleanup in progress

--- main.py
+++ main.py
@@ -14,21 +14,75 @@ color = (255, 255, 255)
 
 # Line thickness of 1 px
 thickness = 1
+DOOR_CHECK_FRAMES = 3
 
+NOT_STARTED = 0
 ENTERING_DOOR = 1
 STILL_IN_DOOR = 2
 EXITING_DOOR = 3
 NOT_IN_DOOR = 4
+PROBABLY_IN_DOOR = 5
+IN_DOOR_ENOUGH_FRAMES = PROBABLY_IN_DOOR + DOOR_CHECK_FRAMES
 
 # how many rooms to compare
 roomsToCompare = 400
 
-# Checks if we think we're in a door transition frame
-# This currently naively checks if the middle part of the screen is dark.  So some rooms might be false positives
-def check_for_door_state(video_frame, previous_frame, current_state):
-    # cut out the hud of the gameplay
-    gameplay_to_process = video_frame[60:, 0:].copy()
+class Video:
+    videoFilename = ""
+    videoStartTime = 0
+    gameplayCrop = [(0, 0), (540, 380)]
 
+
+def check_for_pause(gameplay_to_process, video_id):
+
+    topHud = gameplay_to_process[0:60, 0:240]
+    # cv2.imshow("Hud", topHud)
+
+    gray = cv2.cvtColor(topHud, cv2.COLOR_BGR2GRAY)
+    # thresh = cv2.adaptiveThreshold(gray, 256, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+    ret, thresh = cv2.threshold(gray, 10, 256, cv2.THRESH_BINARY)
+    threshAvg = 256 - np.average(thresh)
+
+    cv2.putText(thresh, str(threshAvg), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
+    # cv2.imshow("hud-thresh-" + video_id, thresh)
+
+    return threshAvg > 245
+
+def check_for_door_equalize(gameplay_to_process, current_state, video_id):
+
+    gray = cv2.cvtColor(gameplay_to_process, cv2.COLOR_BGR2GRAY)
+    # gray_avg = round(np.average(gray))
+
+    thresh = cv2.adaptiveThreshold(gray, 256, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 9, 2)
+    threshAvg = np.average(thresh)
+
+
+    middleBox = thresh[80:-80, 80:-80].copy()
+    middleAvg = np.average(middleBox)
+
+    cv2.putText(thresh, str(threshAvg), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 1, cv2.LINE_AA)
+    cv2.putText(middleBox, str(middleAvg), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1, cv2.LINE_AA)
+    cv2.imshow("thresh-" + video_id, thresh)
+    # cv2.imshow("thresh-mb-" + video_id, middleBox)
+
+
+    # doorTransition = check_for_door_avgStd(gameplay_to_process, current_state)
+    doorTransition = (threshAvg > 243) and (current_state == STILL_IN_DOOR or middleAvg > 253)
+    # if doorTransition and current_state == ENTERING_DOOR:
+    #     currTime = time.time()
+    #     cv2.imwrite("doors/" + video_id + "/door_in_" + str(currTime) + "-g.png", gameplay_to_process)
+    #     cv2.imwrite("doors/" + video_id + "/door_in_" + str(currTime) + ".png", thresh)
+    #     cv2.imwrite("doors/" + video_id + "/door_in_" + str(currTime) + "-m.png", middleBox)
+    #
+    # if not doorTransition and current_state == STILL_IN_DOOR:
+    #     currTime = time.time()
+    #     cv2.imwrite("doors/" + video_id + "/door_out_" + str(currTime) + "-g.png", gameplay_to_process)
+    #     cv2.imwrite("doors/" + video_id + "/door_out_" + str(currTime) + ".png", thresh)
+    #     cv2.imwrite("doors/" + video_id + "/door_out_" + str(currTime) + "-m.png", middleBox)
+
+    return doorTransition
+
+def check_for_door_fivePointAvgStd(gameplay_to_process, current_state):
     # grab the four corners of the screen, which should avoid doors, but include lots
     # of colors from normal rooms
     ulBox = gameplay_to_process[0:100, 0:180]
@@ -67,37 +121,45 @@ def check_for_door_state(video_frame, previous_frame, current_state):
         if any(std > 11.5 for std in stds) or any(avg > 11 for avg in avgs):
             doorTransition = False
 
-    # cv2.imshow('ulBox', ulBox)
-    # cv2.imshow('urBox', urBox)
-    # cv2.imshow('llBox', llBox)
-    # cv2.imshow('lrBox', lrBox)
+    return doorTransition
 
-    # gameplay_prev = previous_frame[60:-60, 60:-60].copy()
+# Checks if we think we're in a door transition frame
+# This currently naively checks if the middle part of the screen is dark.  So some rooms might be false positives
+def check_for_door_state(video_frame, previous_frame, current_state, video_id=""):
+    # cut out the hud of the gameplay
+    gameplay_to_process = video_frame[60:, 0:].copy()
+    doorTransition = check_for_door_equalize(gameplay_to_process, current_state, video_id)
+    doorTransition = doorTransition and check_for_door_fivePointAvgStd(gameplay_to_process, current_state)
 
-    # gray = cv2.cvtColor(gameplay_to_process, cv2.COLOR_BGR2GRAY)
-    # gray_avg = round(np.average(gray))
+    if doorTransition:
+        pause = check_for_pause(video_frame, video_id)
+        if pause and current_state != NOT_STARTED:
+            doorTransition = False
 
-
-    # gray_prev = cv2.cvtColor(gameplay_prev, cv2.COLOR_BGR2GRAY)
-    # gray_avg_prev = round(np.average(gray_prev))
     doorState = NOT_IN_DOOR
     if doorTransition:
-        if (current_state == STILL_IN_DOOR or current_state == ENTERING_DOOR):
-            doorState = STILL_IN_DOOR
-        if current_state == NOT_IN_DOOR:
+        if current_state >= PROBABLY_IN_DOOR and current_state < IN_DOOR_ENOUGH_FRAMES:
+            doorState = current_state + 1
+        elif current_state == IN_DOOR_ENOUGH_FRAMES:
             doorState = ENTERING_DOOR
-    elif current_state == STILL_IN_DOOR and not doorTransition:
-        doorState = EXITING_DOOR
-    elif current_state == EXITING_DOOR:
-        doorState = NOT_IN_DOOR
+        elif current_state == STILL_IN_DOOR or current_state == ENTERING_DOOR or current_state == EXITING_DOOR:
+            doorState = STILL_IN_DOOR
+        else:
+            # cv2.imwrite("doors/door_" + str(time.time()) + "-c.png", video_frame)
+            doorState = PROBABLY_IN_DOOR
     else:
-        #default to the current state
-        doorState = current_state
+        if current_state == STILL_IN_DOOR or current_state == ENTERING_DOOR:
+            doorState = EXITING_DOOR
+        elif current_state == EXITING_DOOR or current_state >= PROBABLY_IN_DOOR:
+            doorState = NOT_IN_DOOR
+        else:
+            #default to the current state
+            doorState = current_state
 
     # in case I want to see what the gray avg is for debugging
     cv2.putText(gameplay_to_process, "Door State: " + str(doorState), (30, 80),
                   cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
-    cv2.imshow('Process', gameplay_to_process)
+    # cv2.imshow('Process ' + video_id, gameplay_to_process)
 
     return doorState
 
@@ -114,12 +176,27 @@ if __name__ == '__main__':
     # videoFiles = ["Zoast-4046.mp4"]
     # videoFiles = ["Behemoth-4056.mp4"]
     # videoFiles = ["Oatsngoats-4146.mp4"]
-    videoFiles = ["Oatsngoats-4146.mp4", "Zoast-4046.mp4", "Behemoth-4056.mp4"]
+    # videoFiles = ["Oatsngoats-4146.mp4", "Zoast-4046.mp4", "Behemoth-4056.mp4"]
+
+    oats4146 = Video()
+    oats4146.videoFilename = "Oatsngoats-4146.mp4"
+    oats4146.gameplayCrop = [(180, 3), (535, 355)]
+    oats4146.videoStartTime = 24
+
+    zoast4046 = Video()
+    zoast4046.videoFilename = "Zoast-4046.mp4"
+    zoast4046.gameplayCrop = [(135, 0), (540, 380)]
+    zoast4046.videoStartTime = 2
+
+    behemoth4056 = Video()
+    behemoth4056.videoFilename = "Behemoth-4056.mp4"
+    behemoth4056.gameplayCrop = [(150, 12), (540, 380)]
+    behemoth4056.videoStartTime = 3
+
+    videos = [oats4146, zoast4046, behemoth4056]
 
     # start times in seconds
-    startMin = 11
-    startTimes = [24 + startMin * 60, 2 + startMin * 60, 3 + startMin * 60]
-    doorDetectionInfo = [-1, -1]
+    startMin = 0
 
     # Rumble
     rumbleCrop = [(140, 0), (540, 380)]
@@ -127,26 +204,22 @@ if __name__ == '__main__':
     # Sloaters
     sloatersCrop = [(0, 30), (375, 380)]
 
-    zoastCrop = [(135, 0), (540, 380)]
-    beheCrop = [(150, 12), (540, 380)]
-    oatsCrop = [(180, 0), (540, 360)]
-
-    gameCropCoords = [oatsCrop, zoastCrop, beheCrop]
-
     roomsCompared = 0
-
+    gameCropCoords = []
     captures = []
-    startFrames = [0] * len(videoFiles)
-    # inRoomTransition = [False] * len(videoFiles)
-    processVideo = [True] * len(videoFiles)
-    lastRoomFrameEnd = [0] * len(videoFiles)
-    lastRoomTimeStr = [""] * len(videoFiles)
-    currentDoorState = [NOT_IN_DOOR] * len(videoFiles)
-    firstFrame = [True] * len(videoFiles)
-    capturedFrames = [None] * len(videoFiles)
+    startTimes = []
+    startFrames = [0] * len(videos)
+    processVideo = [True] * len(videos)
+    lastRoomFrameEnd = [0] * len(videos)
+    lastRoomTimeStr = [""] * len(videos)
+    currentDoorState = [NOT_STARTED] * len(videos)
+    firstFrame = [True] * len(videos)
+    capturedFrames = [None] * len(videos)
 
-    for videoFile in videoFiles:
-        captures.append(cv2.VideoCapture(videoFile))
+    for videoFile in videos:
+        captures.append(cv2.VideoCapture(videoFile.videoFilename))
+        gameCropCoords.append(videoFile.gameplayCrop)
+        startTimes.append(videoFile.videoStartTime + startMin * 60)
 
     fr = int(captures[0].get(cv2.CAP_PROP_FPS))
     for idx, capture in enumerate(captures):
@@ -197,16 +270,16 @@ if __name__ == '__main__':
             currentFrame = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
 
             if not firstFrame[idx]:
-                currentDoorState[idx] = check_for_door_state(gameplay, capturedFrames[idx], currentDoorState[idx])
+                currentDoorState[idx] = check_for_door_state(gameplay, capturedFrames[idx], currentDoorState[idx], str(idx))
             else:
                 firstFrame[idx] = False
 
             if currentDoorState[idx] == EXITING_DOOR:
                 # calculate time
                 roomFrames = (currentFrame - lastRoomFrameEnd[idx])
-                seconds = int(roomFrames / fr)
+                seconds = roomFrames / fr
 
-                if seconds < 3:
+                if seconds < 1.2:
                     currentDoorState[idx] = STILL_IN_DOOR
 
             if currentDoorState[idx] == ENTERING_DOOR:
@@ -214,11 +287,9 @@ if __name__ == '__main__':
                 seconds = int(roomFrames / fr)
                 frames = int(roomFrames) % fr
                 lastRoomTimeStr[idx] = str(seconds) + "s " + str(frames) + "f"
-                cv2.imshow('Door Transition ' + str(idx+1), gameplay)
+                # cv2.imshow('Door Transition ' + str(idx+1), gameplay)
                 lastRoomFrameEnd[idx] = currentFrame
                 processVideo[idx] = False
-
-
 
             cv2.putText(gameplay,
                         "Prev: " + lastRoomTimeStr[idx],
@@ -228,8 +299,9 @@ if __name__ == '__main__':
                         color,
                         thickness,
                         cv2.LINE_AA)
+
             cv2.putText(gameplay,
-                        videoFiles[idx],
+                        videos[idx].videoFilename,
                         (0, 350),
                         font,
                         fontScale * .5,
